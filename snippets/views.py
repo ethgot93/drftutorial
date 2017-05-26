d7afc2517383664fa2bb8caac14014447ccc25fb
ethgot93 <ethgot93@gmail.com>
2017-05-25 23:52:28 -0400

    added view logic to the snippets app

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParsers
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+	"""
+	List all code snippets, or create a new snippet.
+	"""
+	if request.method == 'GET':
+		snippets = Snippets.objects.all()
+		serializer = SnippetSerializer(snippets, many=True)
+		return JsonResponse(serializer.data, safe=False)
+
+
+	elif request.method = 'POST':
+		data = JSONParser().parse(request)
+		serializer = SnippetSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=201)
+		return JsonResponse(serializer.errors, status=400)
